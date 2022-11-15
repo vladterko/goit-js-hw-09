1e9fbe03f6f259aa5f004e925d54a027fb45ea86
Vlad Tereshchenko <vlad.terko@gmail.com>
2022-11-15 18:19:40 +0200

    Adds task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
-console.log('Hello world!');
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const pageBgColor = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+startBtn.addEventListener('click', onStartBtn);
+stopBtn.addEventListener('click', onStopBtn);
+stopBtn.setAttribute('disabled', '');
+
+let timerId = null;
+
+function onStartBtn () {
+    stopBtn.removeAttribute('disabled');
+    startBtn.setAttribute('disabled', '');
+    timerId = setInterval(() => {
+        pageBgColor.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopBtn () {
+    startBtn.removeAttribute('disabled');
+    stopBtn.setAttribute('disabled', '');
+    clearInterval(timerId);
+}
